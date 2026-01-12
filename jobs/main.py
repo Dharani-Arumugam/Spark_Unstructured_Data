19df8541f9fc1da0dce04107a15e3c534b2decae
DharaniArumugam <59991327+Dharani-Arumugam@users.noreply.github.com>
2026-01-12 11:15:44 -0600

    Add files via upload

---
+++ jobs/main.py
@@ -0,0 +1,110 @@
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, DateType
+from pyspark.sql.functions import udf, regexp_replace, initcap
+from config.config import configuration
+from udf_job_bulletins import *
+
+def define_udfs():
+    return{
+        'extract_file_name_udf': udf(extract_file_name, StringType()),
+        'extract_position_udf': udf(extract_position, StringType()),
+        'extract_class_code_udf': udf(extract_class_code, StringType()),
+        'extract_salary_udf': udf(extract_salary, StructType([
+            StructField('salary_start', DoubleType(), True),
+            StructField('salary_end', DoubleType(), True),
+        ])),
+        'extract_start_date_udf': udf(extract_start_date, DateType()),
+        'extract_end_date_udf': udf(extract_end_date, DateType()),
+        'extract_requirements_udf': udf(extract_requirements, StringType()),
+        'extract_notes_udf': udf(extract_notes, StringType()),
+        'extract_duties_udf': udf(extract_duties, StringType()),
+        'extract_selection_udf': udf(extract_selection, StringType()),
+        'extract_experience_length_udf': udf(extract_experience_length, StringType()),
+        'extract_education_length_udf': udf(extract_education_length, StringType()),
+        'extract_application_location_udf': udf(extract_application_location, StringType())
+    }
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("AWS_Spark_Unstructured_Data")\
+           .config('spark.jars.packages',
+                   'org.apache.hadoop:hadoop-aws:3.3.1,'
+                   'com.amazonaws:aws-java-sdk:1.11.469')\
+           .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+           .config("spark.hadoop.fs.s3a.access.key", configuration.get('AWS_ACCESS_KEY')) \
+           .config("spark.hadoop.fs.s3a.secret.key", configuration.get('AWS_SECRET_KEY')) \
+           .config('spark.hadoop.fs.s3a.aws.credentials.provider',
+                'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
+           .getOrCreate()
+    #Adjust the log level to minimize the console output on executors
+
+    text_input_dir = 'file:/opt/spark/input/input_text'
+    json_input_dir = 'file:/opt/spark/input/input_json'
+
+    data_schema = StructType([
+        StructField('filename', StringType(), True),
+        StructField('position', StringType(), True),
+        StructField('classcode', StringType(), True),
+        StructField('salary_start', DoubleType(), True),
+        StructField('salary_end', DoubleType(), True),
+        StructField('start_date', DateType(), True),
+        StructField('end_date', DateType(), True),
+        StructField('req', StringType(), True),
+        StructField('notes', StringType(), True),
+        StructField('duties', StringType(), True),
+        StructField('selection', StringType(), True),
+        StructField('experience_length', StringType(), True),
+        StructField('job_type', StringType(), True),
+        StructField('education_length', StringType(), True),
+        StructField('school_type', StringType(), True),
+        StructField('application_location', StringType(), True),
+    ])
+    udfs = define_udfs()
+    # Read text data
+    jobs_bulletins_df = spark.readStream\
+                             .format('text')\
+                             .option('wholetext', True)\
+                             .load(text_input_dir)
+
+    json_df = spark.readStream.json(json_input_dir,schema=data_schema,multiLine=True)
+
+
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('filename', regexp_replace(udfs['extract_file_name_udf']('value'), r'\r', ''))
+    #jobs_bulletins_df = jobs_bulletins_df.withColumn('value', regexp_replace(regexp_replace('value',r'\n', ' '),r'\r',' '))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('position', initcap(regexp_replace(udfs['extract_position_udf']('value'), r'\r', '')))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('classcode', udfs['extract_class_code_udf']('value'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('salary_start', udfs['extract_salary_udf']('value').getField('salary_start'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('salary_end', udfs['extract_salary_udf']('value').getField('salary_end'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('start_date', udfs['extract_start_date_udf']('value'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('end_date', udfs['extract_end_date_udf']('value'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('req', udfs['extract_requirements_udf']('value'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('notes', udfs['extract_notes_udf']('value'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('duties', udfs['extract_duties_udf']('value'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('selection', udfs['extract_selection_udf']('value'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('experience_length', udfs['extract_experience_length_udf']('value'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('education_length', udfs['extract_education_length_udf']('value'))
+    jobs_bulletins_df = jobs_bulletins_df.withColumn('application_location', udfs['extract_application_location_udf']('value'))
+
+    jobs_bulletins_df = jobs_bulletins_df.select('filename', 'position', 'classcode', 'salary_start', 'salary_end', 'start_date','end_date', 'req','notes','duties','selection', 'experience_length','education_length','application_location')
+
+    json_df = json_df.select('filename', 'position', 'classcode', 'salary_start', 'salary_end', 'start_date','end_date', 'req','notes','duties','selection', 'experience_length','education_length','application_location')
+
+    union_df = jobs_bulletins_df.union(json_df)
+
+
+    def streamWriter(input:DataFrame, checkpointFolder, output):
+        return  ( input.writeStream
+                  .format('parquet')
+                  .option('checkpointLocation', checkpointFolder)
+                  .option('path', output)
+                  .outputMode('append')
+                  .trigger(processingTime='5 seconds')
+                  .start()
+        )
+
+
+    query = streamWriter(union_df, 's3a://unstructured-spark-streaming/checkpoints/',
+                         's3a://unstructured-spark-streaming/data/spark_unstructured')
+
+
+    query.awaitTermination()
+
+    spark.stop()
